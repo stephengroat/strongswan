stephengroat/strongswan

commit 8d96f90a7983d7c48046492aad1c42b3f4691923

vici: Add function to test if an event should be generated
Useful to avoid generating vici messages if they are not needed and their
generation is heavy operation.

Signed-off-by: Timo Teräs <timo.teras@iki.fi>

src/libcharon/plugins/vici/vici_dispatcher.c
@@ -13,6 +13,28 @@
  * for more details.
  */
 
+/*
+ * Copyright (C) 2014 Timo Teräs <timo.teras@iki.fi>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 #include "vici_dispatcher.h"
 #include "vici_socket.h"
 
@@ -446,6 +468,24 @@ METHOD(vici_dispatcher_t, manage_event, void,
 	this->mutex->unlock(this->mutex);
 }
 
+METHOD(vici_dispatcher_t, has_event_listeners, bool,
+	private_vici_dispatcher_t *this, char *name)
+{
+	bool retval = FALSE;
+
+	this->mutex->lock(this->mutex);
+	if (this->events->get(this->events, name))
+	{
+		/* the entry might be getting destroyed, but returning
+		 * false positive is not a problem as a later raise_event
+		 * will check things again. */
+		retval = TRUE;
+	}
+	this->mutex->unlock(this->mutex);
+
+	return retval;
+}
+
 METHOD(vici_dispatcher_t, raise_event, void,
 	private_vici_dispatcher_t *this, char *name, u_int id,
 	vici_message_t *message)
@@ -504,6 +544,7 @@ vici_dispatcher_t *vici_dispatcher_create(char *uri)
 		.public = {
 			.manage_command = _manage_command,
 			.manage_event = _manage_event,
+			.has_event_listeners = _has_event_listeners,
 			.raise_event = _raise_event,
 			.destroy = _destroy,
 		},

src/libcharon/plugins/vici/vici_dispatcher.h
@@ -13,6 +13,28 @@
  * for more details.
  */
 
+/*
+ * Copyright (C) 2014 Timo Teräs <timo.teras@iki.fi>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 /**
  * @defgroup vici_dispatcher vici_dispatcher
  * @{ @ingroup vici
@@ -94,6 +116,17 @@ struct vici_dispatcher_t {
 	 * @param reg			TRUE to register, FALSE to unregister
 	 */
 	void (*manage_event)(vici_dispatcher_t *this, char *name, bool reg);
+
+	/**
+	 * Check if an event has listeners.
+	 *
+	 * This can be used to check if a vici message needs to be generated or not,
+	 * as in some cases the generation can be a heavy operation.
+	 *
+	 * @param name			event name to check
+	 * @return				TRUE if event has listeners
+	 */
+	bool (*has_event_listeners)(vici_dispatcher_t *this, char *name);
 
 	/**
 	 * Raise an event to a specific or all clients registered to that event.

commit afaf5033bd3efca7dde5e50d455421eca297822b

vici: Let has_event_listeners() actually check if clients are registered
Fixes: 8d96f90a7983 ("vici: Add function to test if an event should be
generated")

src/libcharon/plugins/vici/vici_dispatcher.c
@@ -471,15 +471,17 @@ METHOD(vici_dispatcher_t, manage_event, void,
 METHOD(vici_dispatcher_t, has_event_listeners, bool,
 	private_vici_dispatcher_t *this, char *name)
 {
+	event_t *event;
 	bool retval = FALSE;
 
 	this->mutex->lock(this->mutex);
-	if (this->events->get(this->events, name))
+	event = this->events->get(this->events, name);
+	if (event)
 	{
 		/* the entry might be getting destroyed, but returning
 		 * false positive is not a problem as a later raise_event
 		 * will check things again. */
-		retval = TRUE;
+		retval = array_count(event->clients);
 	}
 	this->mutex->unlock(this->mutex);
 